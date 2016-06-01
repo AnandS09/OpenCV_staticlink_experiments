26ee2a9148e486b073b98e0af3655bb57181b9cd
AnandS09 <anandsamajdar09@gmail.com>
2016-06-01 23:01:47 +0530

    More local changes

--- openCVLibrary300/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary300/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\Code\\OpenCV_staticlink_experiments\\openCVLibrary300\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\asamajda\\AndroidStudioProjects\\OpenCV_staticlink_experiments\\openCVLibrary300\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
